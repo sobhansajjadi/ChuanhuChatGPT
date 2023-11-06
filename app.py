afcaa4ce730d5a15837ee3a883ebceca0bdea478
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 17:56:10 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import time
 
 def test(x):
-time.sleep(4)
-return x
+    time.sleep(4)
+    return x
 
 gr.Interface(test, "textbox", "textbox").queue().launch(root_path="/gradio-demo")
