1691a1c6af7fc32af447afba9b8cc065f6f22735
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 14:11:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,18 @@
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, \
-    filters, InlineQueryHandler, Application, CallbackContext, CallbackQueryHandler, ConversationHandler
-from telegram import Update
-import openai
-class TelegramBot:
-    """
-    Class representing a ChatGPT Telegram Bot.
-    """
+from flask import Flask, request, redirect
+import gradio as gr
 
-    def __init__(self):
-        print("HELLO")
-        self.TOKEN = "5921556318:AAFP-IJuj73H5xIe9J147ViVkXpLLQ5BqUc"
-        self.api = "a"
-        pass
-    async def start(self, update: Update, context: CallbackContext):
-        await update.message.reply_text("Welcome to your Telegram bot!")
+# اینجا تابع Gradio خود را تعریف کنید
+def greet(name):
+    return f"Hello {name}!"
 
-    async def prompt(self, update: Update, context: CallbackContext):
-        user_id = update.message.from_user.id
-        message_text = update.message.text
-        messages= [{"role": "user","content": message_text}]
-        print(f"User {user_id} sent: {message_text}")
-        openai.api_key = self.api
-        openai.api_base = "https://api.gateai.ir"
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 
-        response = await openai.ChatCompletion.acreate(
-            model="gpt-3.5-turbo",
-            max_tokens=1000,
-            messages=messages,
-            temperature=0.5,
-            n=1,
-            stream=False
-        )
-        if "choices" in response and "message" in response.choices[0]:  # non-streaming
-            await update.message.reply_text(response.choices[0]["message"]["content"].strip())
-        else:
-            await update.message.reply_text("??")
+# Flask application
+app = Flask(__name__)
 
-
-    async def post_init(self, application: Application) -> None:
-        """
-        Post initialization hook for the bot.
-        """
-
-
-    def main(self):
-
-        application = ApplicationBuilder() \
-            .token(self.TOKEN) \
-            .post_init(self.post_init) \
-            .concurrent_updates(True) \
-            .build()
-
-
-        application.add_handler(CommandHandler("start", self.start))
-        application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
-        application.run_polling()
+@app.route('/', methods=['GET'])
+def home():
+    return iface.launch()
 
 if __name__ == "__main__":
-    tb = TelegramBot()
-    tb.main()
+    app.run(port=7860)  # تغییر دهید به پورت مورد نظر
