b035c95c5ef5b0918c0a99c0e2de83bfd3f6c934
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 13:42:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import os
 
 def get_system_info():
     print(f"Operating System: {os.name}")
-    print(f"Current Working Directory: {os.getcwd()}")
 
 def get_python_version():
     print(f"Python Version: {os.popen('python --version').read()}")
 
-def get_node_version():
-    print(f"Node.js Version: {os.popen('node -v').read()}")
+def start():
+    print("start...")
+
     os.system('python app.py')
-    print("nice♥")
+    print("nice ♥")
     #os.system('python app.py')
 
 
 if __name__ == "__main__":
     get_system_info()
     get_python_version()
-    get_node_version()
+    start()
