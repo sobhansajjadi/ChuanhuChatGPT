bd05e417d3fc4152dc2d11562874be4e9685e6f0
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 17:03:35 +0330

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -261,7 +261,7 @@ def update_doc_config(two_column_pdf):
 
 
 # 处理gradio.launch参数
-server_name = config.get("server_name", "chat.gateai.ir")
+server_name = config.get("server_name", None)
 server_port = config.get("server_port", None)
 if server_name is None:
     if dockerflag:
