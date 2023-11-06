2cf89f8e46d1d76acdba5c721377237789cfa46e
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 19:04:30 +0330

    D

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def start():
 
     #subprocess.run(["python", "ChuanhuChatbot.py"])
     #subprocess.run(["python", "app.py"])
-    os.system('python ChuanhuChatbot.py')
+    os.system('python3 ChuanhuChatbot.py')
     print("nice ♥")
 
 if __name__ == "__main__":
