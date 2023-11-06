3f97822f4ea896de24690d3bf60ee200edbb8009
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 18:56:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import os
 import subprocess
 
-def get_system_info():
-    print(f"Operating System: {os.name}")
-
-def get_python_version():
-    print(f"Python Version: {os.popen('python --version').read()}")
 
 def start():
     print("start...")
@@ -17,6 +12,4 @@ def start():
 
 
 if __name__ == "__main__":
-    get_system_info()
-    get_python_version()
     start()
