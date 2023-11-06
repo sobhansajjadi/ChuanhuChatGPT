43c00ddb30265adb54803bad0054dc0edb6c0165
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 17:55:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,8 @@
 import gradio as gr
+import time
 
-from transformers import pipeline
+def test(x):
+time.sleep(4)
+return x
 
-english_translator = gr.load(name="spaces/gradio/english_translator")
-english_generator = pipeline("text-generation", model="distilgpt2")
-
-
-def generate_text(text):
-    english_text = english_generator(text)[0]["generated_text"]
-    german_text = english_translator(english_text)
-    return english_text, german_text
-
-
-with gr.Blocks() as demo:
-    with gr.Row():
-        with gr.Column():
-            seed = gr.Text(label="Input Phrase")
-        with gr.Column():
-            english = gr.Text(label="Generated English Text")
-            german = gr.Text(label="Generated German Text")
-    btn = gr.Button("Generate")
-    btn.click(generate_text, inputs=[seed], outputs=[english, german])
-    gr.Examples(["My name is Clara and I am"], inputs=[seed])
-
-if __name__ == "__main__":
-    demo.launch()
+gr.Interface(test, "textbox", "textbox").queue().launch(root_path="/gradio-demo")
