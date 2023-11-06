547cf1e967381c2047505ba2c826a399a42c7b1e
sobhansajjadi <pesareirani100@gmail.com>
2023-11-06 18:06:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import gradio as gr
 import time
-
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
+)
 def test(x):
     time.sleep(4)
+    print(x)
     return x
+print("x")
 
 gr.Interface(test, "textbox", "textbox").queue().launch()
